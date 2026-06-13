0b3c5a35c2139a9fd7118e9a4a5c2ad7d10f067e
Selim Soytaş <selimsoytas.0309@gmail.com>
2026-06-13 20:32:13 +0300

    Use enviroment variable for frontend API URL

--- frontend/src/api/WeatherApi.ts
+++ frontend/src/api/WeatherApi.ts
@@ -1,7 +1,7 @@
 import type { Weather } from "../../types/Weather";
 import { weatherSchema } from "../validation/weatherValidation";
 
-const BASE_URL = "http://localhost:7070/";
+const BASE_URL = import.meta.env.VITE_API_URL ?? "http://localhost:7070/";
 
 export default async function getWeather(city: string): Promise<Weather> {
   const url = `${BASE_URL}weather?city=${encodeURIComponent(city)}`;
